1c5b54ba967613968ece2ac3dd8c84d99fe40e24
lordprana <matthew.gaba.2@gmail.com>
2018-03-20 14:21:11 -0400

    Feat(OCR): Add OCR module

---
+++ components/OCR.js
@@ -0,0 +1,96 @@
+import React from 'react';
+
+import { Text, View, TouchableOpacity } from 'react-native';
+import { Camera, Permissions } from 'expo';
+import axios from 'axios';
+import styles from '../styles';
+import { backEndAddress } from '../config';
+
+
+export default class OCR extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      hasCameraPermission: null,
+      type: Camera.Constants.Type.back,
+    };
+
+    this.camera = null;
+
+    this.snap = this.snap.bind(this);
+  }
+
+  async componentWillMount() {
+    const { status } = await Permissions.askAsync(Permissions.CAMERA);
+    this.setState({ hasCameraPermission: status === 'granted' });
+  }
+
+  snap() {
+    if (this.camera) {
+      let now = new Date();
+      this.camera.takePictureAsync({ base64: true, quality: 0.1 })
+        .then(photo => {
+          console.log('Took a picture');
+          console.log(photo);
+          // TODO Add museumId at query parameter
+          return axios.post(`${backEndAddress}/api/identify-piece-from-plaque-image`, {
+            requests: [
+              {
+                image: {
+                  content: photo.base64
+                },
+                features: [
+                  {
+                    type: 'TEXT_DETECTION'
+                  }
+                ]
+              }
+            ]
+          });
+        })
+        .then(res => {
+          // TODO Add navigation to Piece forum here
+        })
+        .catch(console.error.bind(console));
+    }
+}
+
+  render() {
+    const { hasCameraPermission } = this.state;
+    if (hasCameraPermission === null) {
+      return <View />;
+    } else if (hasCameraPermission === false) {
+      return <Text>No access to camera</Text>;
+    } else {
+      return (
+        <View style={{ flex: 1 }}>
+          <Camera style={{ flex: 1 }} type={this.state.type} ref={ref => {this.camera = ref;}} >
+            <Text>
+              Take a picture of the art plaque to join the conversation about the piece
+            </Text>
+            <View
+              style={{
+                flex: 1,
+                backgroundColor: 'transparent',
+                flexDirection: 'row',
+              }}>
+              <TouchableOpacity
+                style={{
+                  flex: 0.1,
+                  alignSelf: 'flex-end',
+                  alignItems: 'center',
+                }}
+                onPress={this.snap}>
+                <Text
+                  style={{ fontSize: 18, marginBottom: 10, color: 'white' }}>
+                  {' '}Photo{' '}
+                </Text>
+              </TouchableOpacity>
+            </View>
+          </Camera>
+        </View>
+      );
+    }
+  }
+}
