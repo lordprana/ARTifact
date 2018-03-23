819ed69e0f409c8811f0e37b859aaa83bd653b49
lordprana <matthew.gaba.2@gmail.com>
2018-03-23 12:34:19 -0400

    Fix(app): Disable phone rotation

--- App.js
+++ App.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import Main from './components/Main';
+import Expo from 'expo';
 import { Provider } from 'react-redux';
 import store from './store'
 
 export default class App extends React.Component {
+  componentDidMount() {
+    Expo.ScreenOrientation.allow(Expo.ScreenOrientation.Orientation.PORTRAIT);
+  }
   render() {
     return (
       <Provider store={store}>
