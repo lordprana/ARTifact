9761a2d059798fa22e2617049e9e7dfb6c287437
lordprana <matthew.gaba.2@gmail.com>
2018-03-23 15:50:57 -0400

    Fix(LoginScreen): Fix navigation when user is not logged in

--- components/LoginScreen.js
+++ components/LoginScreen.js
@@ -1,12 +1,12 @@
-import React from 'react'
-import { Text, View, Image } from 'react-native'
+import React from 'react';
+import { Text, View, Image } from 'react-native';
 import { Permissions, Location } from 'expo';
-import styles from '../styles'
-import LoginButton from './LoginButton'
-import BackgroundImage from './BackgroundImage'
+import styles from '../styles';
+import LoginButton from './LoginButton';
+import BackgroundImage from './BackgroundImage';
 import { connect } from 'react-redux';
 import { NavigationActions } from 'react-navigation';
-import { getUuidFromStorage, getUserInfo } from '../store/user'
+import { getUuidFromStorage, getUserInfo } from '../store/user';
 
 const LoginScreen = (props) => {
   return (
@@ -14,7 +14,7 @@ const LoginScreen = (props) => {
       <View style={styles.container}>
         <Text style={styles.titleText}>Welcome to ARTifact</Text>
         {
-          props.renderLoginButton && <LoginButton />
+          props.renderLoginButton && <LoginButton navigation={props.navigation} />
         }
       </View>
     </BackgroundImage>
@@ -23,19 +23,18 @@ const LoginScreen = (props) => {
 
 class Auth extends React.Component {
   constructor() {
-    super()
+    super();
     this.state = {
-      loading: false,
+      loading: true,
       hasLocationPermission: null
-    }
-    this.getLocation = this.getLocation.bind(this)
+    };
+    this.getLocation = this.getLocation.bind(this);
   }
 
   componentWillMount() {
-    this.setState({ loading: true })
     this.props.getUuidFromStorage()
       .then(result => {
-        this.setState({ loading: false })
+        this.setState({ loading: false });
         if (result) {
           this.props.getUserInfo()
             .then(result => {
@@ -51,11 +50,11 @@ class Auth extends React.Component {
                       )
                     ]
                   })
-              )
+              );
             }
-            )
+            );
         }
-      })
+      });
   }
   async getLocation() {
     const { status } = await Permissions.askAsync(Permissions.LOCATION);
@@ -64,22 +63,24 @@ class Auth extends React.Component {
   }
   render() {
     return (
-      this.props.loading
+      this.state.loading
         ? <LoginScreen renderLoginButton={false} />
         : !this.props.uuid
-          ? <LoginScreen renderLoginButton={true} />
+          ? <LoginScreen
+            renderLoginButton={true}
+            navigation={this.props.navigation} />
           : <LoginScreen renderLoginButton={false} />
-    )
+    );
   }
 }
 const mapState = state => ({
   uuid: state.user.uuid,
   name: state.user.name,
   pictureUrl: state.user.pictureUrl
-})
+});
 const mapDispatch = dispatch => ({
   getUuidFromStorage: () => dispatch(getUuidFromStorage()),
   getUserInfo: () => dispatch(getUserInfo()),
-})
+});
 
-export default connect(mapState, mapDispatch)(Auth)
+export default connect(mapState, mapDispatch)(Auth);
